4af7f3cc3fc6ff5b1f32ab6db93727e44eb3f28d
Michael Tracy <michael@luckywabbit.com>
2016-02-07 22:20:23 -0500

    Added jquery, bootstrap, google font, and style.css

--- index.php
+++ index.php
@@ -1,5 +1,32 @@
-<h1>Hello World</h1>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>DWA - Hello World</title>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-7s5uDGW3AHqw6xtJmNNtr+OBRJUlgkNJEo78P4b0yRw= sha512-nNo+yCHEyn0smMxSswnf/OnX6/KwJuZTlNZBjauKhTK0c+zT+q5JOCx0UFhXQ6rJR9jg6Es8gPuD2uZcYDLqSw==" crossorigin="anonymous">/
 
-<h2>Welcome to my site</h2>
+<link href='https://fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'>
 
-<img src="img/bun.jpg"/>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+<div class="container-fluid">
+  <div class="row">
+    <header>
+      <div class="col-md-12 branding">
+        <h1>Lucky Wabbit</h1>
+      </div>
+    </header>
+  </div>
+  <div class="row">
+    <div class="col-md-12">
+      <h2>Hello World</h2>
+      <h3>Welcome to my site</h3>
+      <img src="img/bun.jpg"/> </div>
+
+  </div>
+</div>
+</body>
+</html>
