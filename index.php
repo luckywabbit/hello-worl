a7e97142b6c0d06031c1a47927631ddcfffa1e5a
Michael Tracy <michael@luckywabbit.com>
2016-02-08 07:24:05 -0500

    Remove stray '/' from HTML

--- index.php
+++ index.php
@@ -4,13 +4,10 @@
 <meta charset="utf-8">
 <title>DWA - Hello World</title>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-7s5uDGW3AHqw6xtJmNNtr+OBRJUlgkNJEo78P4b0yRw= sha512-nNo+yCHEyn0smMxSswnf/OnX6/KwJuZTlNZBjauKhTK0c+zT+q5JOCx0UFhXQ6rJR9jg6Es8gPuD2uZcYDLqSw==" crossorigin="anonymous">/
-
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-7s5uDGW3AHqw6xtJmNNtr+OBRJUlgkNJEo78P4b0yRw= sha512-nNo+yCHEyn0smMxSswnf/OnX6/KwJuZTlNZBjauKhTK0c+zT+q5JOCx0UFhXQ6rJR9jg6Es8gPuD2uZcYDLqSw==" crossorigin="anonymous">
 <link href='https://fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'>
-
 <link rel="stylesheet" type="text/css" href="style.css">
 </head>
-
 <body>
 <div class="container-fluid">
   <div class="row">
@@ -24,8 +21,8 @@
     <div class="col-md-12">
       <h2>Hello World</h2>
       <h3>Welcome to my site</h3>
-      <img src="img/bun.jpg"/> </div>
-
+      <img src="img/bun.jpg"/>
+    </div>
   </div>
 </div>
 </body>
